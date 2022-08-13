6203075754cffdefb65d3cfb779b29764fee1925
Christopher <chip@projecterror.dev>
2022-08-13 14:31:16 -0700

    Update server.lua

--- server/server.lua
+++ server/server.lua
@@ -3,7 +3,6 @@ RegisterNetEvent("npwd:esx-garage:getVehicles", function()
 	local xPlayer  = ESX.GetPlayerFromId(src)
 
 	local result = MySQL.query.await('SELECT * from owned_vehicles WHERE owner = ?', {xPlayer.identifier})
-	print(json.encode(result))
 
 	TriggerClientEvent('npwd:esx-garage:sendVehicles', src, result)
 end)
